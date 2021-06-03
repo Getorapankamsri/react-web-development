fded8520885e8e81c858c6055b6ff421db6508b3
Geteg <45251189+Getorapankamsri@users.noreply.github.com>
2021-06-03 13:04:56 +0700

    ฝึกหัด

---
+++ javascript/es5_es6/main.js
@@ -0,0 +1,19 @@
+// let name1 = "Get";
+
+// if (true) {
+//     let name1 = "Phoom";
+//     console.log(name1);
+// }
+
+// console.log(name1);
+
+// const fruit = "banana";
+
+// const person = {};
+// person.name = "Get";
+// console.log(person);
+
+const str = 'In ES6 ' +
+    'Multi-line is okay';
+
+console.log(str)
